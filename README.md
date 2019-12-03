38d042c34d8dc6b7ce677f62656ba3e94a2b498a
hanqing <18523978636@163.com>
2019-12-03 09:44:42 -0500

    12.3

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SpeakerVerification
+
+Only enroll 1 utterance
+Only use 1 frame to Verify
+
+pros: Fast
+cons: May choose bad frame, e.g. jianzhi
+
+
+Next: use maltiple continuous frames
+Change frequency resolution
+
+
+
--- slidesWindow.py
+++ slidesWindow.py
@@ -30,52 +30,65 @@ def verify(speaker, wavs, threshold):
     for wav in wavs:
         test_mfcc, test_idx = get_features(wav)
         sim = cosine_similarity(model, test_mfcc[1:, test_idx].reshape(1, -1))
+        print("Speaker Model is {}, test voice from : {}\n, sim : {}\n".format(speaker, wav[:-6], sim))
         if(speaker == wav[:-6]):
             if(sim < threshold):
                 fail_reject += 1
         else:
             if(sim > threshold):
                 fail_accept += 1
-    print("Speaker Model is {}, threshold : {}\n, FAR : {}/24, FRR : {}/6\n".format(speaker, threshold, fail_accept, fail_reject))
-    return threshold, fail_accept*100/24, fail_reject*100/6
+    print("Speaker Model is {}, threshold : {}\n, FAR : {}/12, FRR : {}/6\n".format(speaker, threshold, fail_accept, fail_reject))
+    return threshold, fail_accept*100/12, fail_reject*100/6
+
 
 if __name__ == '__main__':
-    # file = 'xiao-hig/2.wav'
-    # mfcc, idx = get_features(file)
-    #
-    # file1 = 'xiao-hig/6.wav'
-    # mfcc1, idx1 = get_features(file1)
-    # model = mfcc[1:, idx].reshape(1,-1)
-    # print(model, mfcc1[:, idx1].reshape(1,-1))
-    # sim = cosine_similarity(model, mfcc1[1:,idx1].reshape(1,-1))
-    # print(sim)
-    #
-    #
-    # file2 = 'hanqing-hig/1.wav'
-    # mfcc2, idx2 = get_features(file2)
-    # print(model, mfcc2[1:, idx2].reshape(1,-1))
-    # sim1 = cosine_similarity(model, mfcc2[1:, idx2].reshape(1, -1))
-    # print(sim1)
 
     wavs = []
     dataset = [dirs for dirs in os.listdir('.') if (dirs[-4:] == ("-hig"))]
+    print(dataset)
     for speaker in dataset:
         for wav in os.listdir(speaker):
             wavs.append(os.path.join(speaker, wav))
 
-    thes =[]
-    FARs = []
-    FRRs = []
-    for threshold in np.arange(0.5,1.0,0.025):
-        the, FAR, FRR = verify("liuli-hig", wavs, threshold)
-        thes.append(the)
-        FARs.append(FAR)
-        FRRs.append(FRR)
-    plt.plot(thes, FARs, marker='o', label = "FAR", markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
-    plt.plot(thes, FRRs, marker='', label = "FRR", color='olive', linewidth=2)
-    # plt.plot('x', 'y3', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="toto")
+    # thes = []
+    # FARs = []
+    # FRRs = []
+    # for threshold in np.arange(0.5, 1.0, 0.025):
+    #     the, FAR, FRR = verify('Nick-hig', wavs, threshold)
+    #     thes.append(the)
+    #     FARs.append(FAR)
+    #     FRRs.append(FRR)
+    # plt.plot(thes, FARs, marker='o', label = "FAR", markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
+    # plt.plot(thes, FRRs, marker='', label = "FRR", color='olive', linewidth=2)
+
+
+    for idx, speaker in enumerate(dataset):
+        thes = []
+        FARs = []
+        FRRs = []
+        for threshold in np.arange(0.5,1.0,0.025):
+            the, FAR, FRR = verify(speaker, wavs, threshold)
+            thes.append(the)
+            FARs.append(FAR)
+            FRRs.append(FRR)
+        FARs = np.array(FARs)
+        FRRs = np.array(FRRs)
+        if idx == 0:
+            FARss = FARs
+            FRRss = FRRs
+        else:
+            FARss = FARss + FARs
+            FRRss = FRRss + FRRs
+    
+
+
+    plt.plot(thes, FARss/3, marker='o', label = "FAR", markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
+    plt.plot(thes, FRRss/3, marker='', label = "FRR", color='olive', linewidth=2)
+
+
     plt.legend()
     plt.xlabel("Threshold")
     plt.ylabel("%")
     plt.tick_params(labelright=True)
+    plt.title('All')
     plt.show()
