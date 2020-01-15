cc40290cd07f96b1dc67476b00ba74583fdafb4e
Guo <guohanqi@msu.edu>
2020-01-15 15:46:18 -0500

    research

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ import librosa.display
 from IPython.display import Audio
 import random
 from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.patches as patches
 
 
 def readfile(file):
@@ -44,14 +45,20 @@ def gen_mel(sr, nfft, n_mels):
     return mel_basis
 
 
-def get_mfcc(mel_basis, Xdb, sr, n_mfcc, log=True):
+def get_mfcc(mel_basis, max_idx, Xdb, sr, n_mfcc, log=True):
     dot_result = np.dot(mel_basis, Xdb)
     if(log):
         dot_result = np.log10(dot_result + 1e-6)
     librosa.display.specshow(dot_result, sr=sr, x_axis='time', y_axis='hz')
     plt.show()
+
     mfccs = librosa.feature.mfcc(S = dot_result, sr=sr, n_mfcc=n_mfcc)
+    fig, ax = plt.subplots(1)
     librosa.display.specshow(mfccs, x_axis='time')
+    x_axis_res = 17.35/Xdb.shape[0]
+    y_axis_res = 10/Xdb.shape[1]
+    rect = patches.Rectangle((x_axis_res*max_idx, y_axis_res*50), 0.01, 3, linewidth=3, edgecolor='y', facecolor='none')
+    ax.add_patch(rect)
     plt.show()
     return mfccs
 
@@ -75,17 +82,25 @@ def get_fri_indics(Xdb):
     max = 0
     max_idx = 0
     for idx, col in enumerate(Xdb.T):
-        high_sum = sum(col[150:750])
+        high_sum = sum(col[50:600])  # convert this value to MFCC coordinates
         if(high_sum > max):
             max = high_sum
             max_idx = idx
     return max_idx
 
+def get_fri_width(Xdb, max_idx):
+    # similarity function to compare look forward and look backward from max_idx
+    for idx, col in enumerate(Xdb.T):
+        pass
+
+
+
 
 if __name__ == '__main__':
-    sr, samples = readfile('jianzhi-hig/3.wav')
+    sr, samples = readfile('jianzhi-hig/2.wav')
     Xdb = get_stft(sr=sr, samples=samples)
+    print(Xdb.shape)
     max_idx = get_fri_indics(Xdb)
     mel_basis = gen_mel(48000, 2048, 10)
-    get_mfcc(mel_basis, Xdb, 192000, 10, log=True)
+    get_mfcc(mel_basis, max_idx, Xdb, 192000, 10, log=True)
     print(max_idx)
